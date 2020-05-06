2399dcd5147be2b3d65fb2fd7fa5769f561b41a2
Mitusha <aryamitusha@gmail.com>
2020-05-06 12:07:31 +0530

    New

---
+++ Leetcode-JS/min_sorted_array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ /**
+First find the min, reduce iterations
+1. Binary Search
+2. Set boundaries from both the sides of the array to find the lowerst one
+3. start from n =l and R= nums.length-1
+ */
+ var findMin = function(nums) {
+    let left = 0;
+    let right =  nums.length-1;
+     while ( left < right ){
+         let midPoint= Math.floor((left+right)/2);
+         if(nums[midPoint] > nums[right]) left= midPoint+1;
+         else right = midPoint
+     }
+     return nums[left]
+ };
