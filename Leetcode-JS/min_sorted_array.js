a3c3672e29293ef1c82c5cb9fd37efa36a1128dd
Mitusha <aryamitusha@gmail.com>
2020-05-06 12:08:36 +0530

    ..

--- Leetcode-JS/min_sorted_array.js
+++ Leetcode-JS/min_sorted_array.js
@@ -7,6 +7,7 @@ First find the min, reduce iterations
 1. Binary Search
 2. Set boundaries from both the sides of the array to find the lowerst one
 3. start from n =l and R= nums.length-1
+4. Math.floor() function returns the largest integer less than or equal to a given number.
  */
  var findMin = function(nums) {
     let left = 0;
