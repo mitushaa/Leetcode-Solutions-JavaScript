d1a46ccc9dafdc86991d4f904a6257b85ac3b391
Mitusha <aryamitusha@gmail.com>
2020-05-22 09:22:26 +0530

    Update create-array.js

---
+++ Arrays/creating-array.js
@@ -0,0 +1,36 @@
+//Simple Way to Create an Array
+
+var numbers = [];
+//Array is of length0 here
+print(numbers.length);
+
+//Creating an array to declare an array variable with a set of elements inside []element
+var numbers = [1, 2, 3, 4, 5, 6];
+//Array is of length 5 here
+print(numbers.length);
+
+//Creating Array using Array Constructor
+var numbers= new Array();
+print(numbers.length);
+
+var numbers= new Array(1, 2, 3, 4, 5);
+print(numbers.length);
+
+var numbers= new Array(10);
+print(numbers.length);
+
+var objects = [1, "Joe", true, null];
+
+//We can verify that an object is an array by calling the Array.isArray() function, like this:
+var numbers = 3;
+var arr = [7,4,1776];
+print(Array.isArray(number)); // displays false
+print(Array.isArray(arr)); // displays trueWe can verify that an object is an array by calling the Array.isArray() function, like this:
+
+
+var numbers = 3;
+var arr = [7,4,1776];
+print(Array.isArray(number)); // displays false
+print(Array.isArray(arr)); // displays true
+
+//
