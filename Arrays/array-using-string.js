c2f3c505fcd0caf06d6c78e00f24ca37965907b4
Mitusha <aryamitusha@gmail.com>
2020-05-22 17:12:14 +0530

    Update new changes

--- Arrays/array-using-string.js
+++ Arrays/array-using-string.js
@@ -14,3 +14,12 @@ for(var i =0; i<10; ++i){
   nums[i]= i+1;
 }
 var samenums = nums;
+
+//Assigning one array to another array by shallow copy method
+var nums = [];
+for (var i = 0; i < 100; ++i) {
+ nums[i] = i+1;
+}
+var samenums = nums;
+nums[0] = 400;
+print(samenums[0]); // displays 400
