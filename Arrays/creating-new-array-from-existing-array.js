3e8261e44b7fd40cf314dd6a308c6749ca9da152
Mitusha <aryamitusha@gmail.com>
2020-05-22 19:29:34 +0530

    Update creating-new-array-from-existing-array.js

--- Arrays/creating-new-array-from-existing-array.js
+++ Arrays/creating-new-array-from-existing-array.js
@@ -6,3 +6,11 @@ var itDiv = cis.concat(dmp);
 print(itDiv);
 itDiv = dmp.concat(cisDept);
 print(itDiv);
+
+//The splice() function creates a new array from the contents of an existing array. The arguments to the function are the starting position for taking the splice and the number of elements to take from the existing array.
+
+var itDiv = ["Mike","Clayton","Terrill","Raymond","Cynthia","Danny","Jennifer"];
+var dmpDept = itDiv.splice(3,3);
+var cisDept = itDiv;
+print(dmpDept); // Raymond,Cynthia,Danny
+print(cisDept); // Mike,Clayton,Terrill,Jennifer
