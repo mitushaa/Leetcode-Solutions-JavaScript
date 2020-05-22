4974234977ec4a7b6a3688b43c5e8e32ba34e70a
Mitusha <aryamitusha@gmail.com>
2020-05-22 11:22:01 +0530

    Update

--- Arrays/deep-copy-array.js
+++ Arrays/deep-copy-array.js
@@ -4,3 +4,13 @@ function copy(arr1, arr2){
     arr2[i] = arr1[i];
 }
 }
+
+//The following code fragment produces the expected result:
+var nums = [];
+for (var i= 0; i <100; ++i){
+  nums[i]= i+1;
+}
+var samenums = [];
+copy(nums, samenums);
+nums[0]= 400;
+print(samenums[0]);
