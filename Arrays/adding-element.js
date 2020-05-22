fe18d20b5e1009001474e0164fc9035a5431ec01
Mitusha <aryamitusha@gmail.com>
2020-05-22 20:21:50 +0530

    Update adding-element.js

--- Arrays/adding-element.js
+++ Arrays/adding-element.js
@@ -10,3 +10,26 @@ var nums = [1,2,3,4,5];
 print(nums); // 1,2,3,4,5
 nums[nums.length] = 6;
 print(nums); // 1,2,3,4,5,6
+
+//Adding element without using mutator (inefficient)
+
+var nums = [2,3,4,5];
+var newnum = 1;
+var N = nums.length;
+for (var i = N; i >= 0; --i) {
+ nums[i] = nums[i-1];
+}
+nums[0] = newnum;
+print(nums); // 1,2,3,4,5
+
+//The mutator function for adding array elements to the beginning of an array is unshift(). Here is how the function works:
+var nums = [2,3,4,5];
+print(nums); // 2,3,4,5
+var newnum = 1;
+nums.unshift(newnum);
+print(nums); // 1,2,3,4,5
+nums = [3,4,5];
+//Adding mutliple elements
+nums.unshift(newnum,1,2);
+print(nums); // 1,2,3,4,5
+//The second call to unshift() demonstrates that you can add multiple elements to an array with one call to the function.
